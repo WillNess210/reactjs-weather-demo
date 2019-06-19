f04cd4f2c0e1e65de1ffdd017634925181ae70d2
WillNess210 <willness210@gmail.com>
2019-06-19 08:06:32 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# weather-project
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/weather-project)
---
+++ index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Header location = "Boulder"/>
+        <WeatherCard day = "Thu" tempLow = "67" tempHigh = "89"/>
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render(){
+    return(
+      <h1> Weather for {this.props.location} </h1>
+    );
+  };
+}
+
+class WeatherCard extends Component{
+  render(){
+    return(
+      <div class = "weatherCard">
+        <div class = "weatherDay">{this.props.day}</div>
+        <div class = "weatherIcon"> </div>
+        <div class = "weatherTemps">
+          {this.props.tempLow}°   {this.props.tempHigh}°
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
